0ef77c7e6c154181e222e63f9aaff595b7290f82
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-05-04 19:44:20 -0700

    test

--- tools/build/build.ps1
+++ tools/build/build.ps1
@@ -30,11 +30,8 @@ if ( $ENABLE_CARGO_TEST -eq 'true' ) {
 cp ${pwd}/ffmpeg/bin/*.dll ./target/release
 echo $(tree ./target/release)
 
-
 # sign the binary
-$passcode = "${PFX_CERTIFICATION_PASSWORD}"
-echo "code:${passcode}..."
-$password = ConvertTo-SecureString -String $passcode -Force -AsPlainText
+$password = ConvertTo-SecureString -String $PFX_CERTIFICATION_PASSWORD -Force -AsPlainText
 $cert = Get-PfxCertificate -FilePath ./tools/build/MyCert.pfx -Password $password
 Set-AuthenticodeSignature -FilePath ./target/release/${EXECUTABLE_CHECK_NAME}.exe -Certificate $cert -TimeStampServer http://timestamp.digicert.com -HashAlgorithm SHA256
 
@@ -45,19 +42,23 @@ mkdir -p ./${EXECUTABLE_NAME}_windows/assets
 mv ../../assets/ui ./${EXECUTABLE_NAME}_windows/assets/
 mv ../../assets/version_files/${SYSTEM}/levels.json ./${EXECUTABLE_NAME}_windows/
 
-mv ${EXECUTABLE_NAME}.exe ${EXECUTABLE_NAME}_windows/
+# mv ${EXECUTABLE_NAME}.exe ${EXECUTABLE_NAME}_windows/
+mv *.exe ${EXECUTABLE_NAME}_windows/
 mv *.dll ${EXECUTABLE_NAME}_windows/libs/
 Compress-Archive -Path ${EXECUTABLE_NAME}_windows ${EXECUTABLE_NAME}_windows.zip
-mv ${EXECUTABLE_CHECK_NAME}.exe ${EXECUTABLE_NAME}_windows/
+# mv ${EXECUTABLE_CHECK_NAME}.exe ${EXECUTABLE_NAME}_windows/
 
 # Test the Rust program
 if ( $ENABLE_PROGRAM_CHECK -eq 'true'){
+  echo "Running the Check program..."
   ./${EXECUTABLE_NAME}_windows/${EXECUTABLE_CHECK_NAME}.exe
   $exit_code = $LASTEXITCODE
   if ($exit_code -ne 0) {
     Write-Output "Rust program failed with exit code $exit_code"
     exit 1
   }
+} else {
+  echo "Skipping the Check program..."
 }
 
 # delete the cube-infinifold_windows directory
