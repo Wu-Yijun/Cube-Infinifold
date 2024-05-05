ac6e7007308c25be5f8e47977f8ece260c7aea4b
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-05-04 19:33:46 -0700

    test

--- tools/build/build.ps1
+++ tools/build/build.ps1
@@ -1,24 +1,23 @@
-# $ENV_ENABLE_CARGO_BUILD = ${{ env.ENABLE_CARGO_BUILD }}
-# $ENV_ENABLE_CARGO_TEST = ${{ env.ENABLE_CARGO_TEST }}
-# $ENV_ENABLE_PROGRAM_CHECK = ${{ env.ENABLE_PROGRAM_CHECK }}
-# $ENV_EXECUTABLE_NAME = ${{ env.EXECUTABLE_NAME }}
-# $ENV_EXECUTABLE_CHECK_NAME = ${{ env.EXECUTABLE_CHECK_NAME }}
-# $ENV_SYSTEM = 'windows'
-# $ENV_PFX_CERTIFICATION_PASSWORD = ${{ secrets.PFX_CERTIFICATION_PASSWORD }}
-
+$ENABLE_CARGO_BUILD = $env:ENABLE_CARGO_BUILD
+$ENABLE_CARGO_TEST = $env:ENABLE_CARGO_TEST
+$ENABLE_PROGRAM_CHECK = $env:ENABLE_PROGRAM_CHECK
+$EXECUTABLE_NAME = $env:EXECUTABLE_NAME
+$EXECUTABLE_CHECK_NAME = $env:EXECUTABLE_CHECK_NAME
+$SYSTEM = $env:ENV_SYSTEM
+$PFX_CERTIFICATION_PASSWORD = $env:SECRET_PFX_CERTIFICATION_PASSWORD
 
 # Build the project
-if ( $ENV_ENABLE_CARGO_BUILD -eq 'true' ) {
+if ( $ENABLE_CARGO_BUILD -eq 'true' ) {
   cargo build --release
-  echo . > ./target/release/${ENV_EXECUTABLE_NAME}.exe
+  echo . > ./target/release/${EXECUTABLE_NAME}.exe
 } else {
   mkdir -p ./target/release
-  echo . > ./target/release/${ENV_EXECUTABLE_NAME}.exe
-  echo . > ./target/release/${ENV_EXECUTABLE_CHECK_NAME}.exe
+  echo . > ./target/release/${EXECUTABLE_NAME}.exe
+  echo . > ./target/release/${EXECUTABLE_CHECK_NAME}.exe
 }
 
 # Cargo Test
-if ( $ENV_ENABLE_CARGO_TEST -eq 'true' ) {
+if ( $ENABLE_CARGO_TEST -eq 'true' ) {
   cargo test
 }
 
@@ -27,27 +26,27 @@ cp ${pwd}/ffmpeg/bin/*.dll ./target/release
 Tree ./target/release
 
 # sign the binary
-$passcode = "${ENV_PFX_CERTIFICATION_PASSWORD}"
+$passcode = "${PFX_CERTIFICATION_PASSWORD}"
 echo "code:${passcode}..."
 $password = ConvertTo-SecureString -String $passcode -Force -AsPlainText
 $cert = Get-PfxCertificate -FilePath ./tools/build/MyCert.pfx -Password $password
-Set-AuthenticodeSignature -FilePath ./target/release/${ENV_EXECUTABLE_CHECK_NAME}.exe -Certificate $cert -TimeStampServer http://timestamp.digicert.com -HashAlgorithm SHA256
+Set-AuthenticodeSignature -FilePath ./target/release/${EXECUTABLE_CHECK_NAME}.exe -Certificate $cert -TimeStampServer http://timestamp.digicert.com -HashAlgorithm SHA256
 
 # copy and compress the binary and library into a zip file
 cd ./target/release
-mkdir -p ./${ENV_EXECUTABLE_NAME}_windows/libs
-mkdir -p ./${ENV_EXECUTABLE_NAME}_windows/assets
-mv ../../assets/ui ./${ENV_EXECUTABLE_NAME}_windows/assets/
-mv ../../assets/version_files/${ENV_SYSTEM}/levels.json ./${ENV_EXECUTABLE_NAME}_windows/
+mkdir -p ./${EXECUTABLE_NAME}_windows/libs
+mkdir -p ./${EXECUTABLE_NAME}_windows/assets
+mv ../../assets/ui ./${EXECUTABLE_NAME}_windows/assets/
+mv ../../assets/version_files/${SYSTEM}/levels.json ./${EXECUTABLE_NAME}_windows/
 
-mv ${ENV_EXECUTABLE_NAME}.exe ${ENV_EXECUTABLE_NAME}_windows/
-mv *.dll ${ENV_EXECUTABLE_NAME}_windows/libs/
-Compress-Archive -Path ${ENV_EXECUTABLE_NAME}_windows ${ENV_EXECUTABLE_NAME}_windows.zip
-mv ${ENV_EXECUTABLE_CHECK_NAME}.exe ${ENV_EXECUTABLE_NAME}_windows/
+mv ${EXECUTABLE_NAME}.exe ${EXECUTABLE_NAME}_windows/
+mv *.dll ${EXECUTABLE_NAME}_windows/libs/
+Compress-Archive -Path ${EXECUTABLE_NAME}_windows ${EXECUTABLE_NAME}_windows.zip
+mv ${EXECUTABLE_CHECK_NAME}.exe ${EXECUTABLE_NAME}_windows/
 
 # Test the Rust program
-if ( $ENV_ENABLE_PROGRAM_CHECK -eq 'true'){
-  ./${ENV_EXECUTABLE_NAME}_windows/${ENV_EXECUTABLE_CHECK_NAME}.exe
+if ( $ENABLE_PROGRAM_CHECK -eq 'true'){
+  ./${EXECUTABLE_NAME}_windows/${EXECUTABLE_CHECK_NAME}.exe
   $exit_code = $LASTEXITCODE
   if ($exit_code -ne 0) {
     Write-Output "Rust program failed with exit code $exit_code"
@@ -56,7 +55,7 @@ if ( $ENV_ENABLE_PROGRAM_CHECK -eq 'true'){
 }
 
 # delete the cube-infinifold_windows directory
-Remove-Item -Path ./${ENV_EXECUTABLE_NAME}_windows -Recurse -Force
+Remove-Item -Path ./${EXECUTABLE_NAME}_windows -Recurse -Force
 
 # decompress the zip file to root
-Expand-Archive -Path ${ENV_EXECUTABLE_NAME}_windows.zip -Destination ../../
+Expand-Archive -Path ${EXECUTABLE_NAME}_windows.zip -Destination ../../
