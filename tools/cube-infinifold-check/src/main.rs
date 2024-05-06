c0e5b1112354eb790b150da707e632e892b0c199
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-05-06 16:32:24 -0700

    upload

--- tools/cube-infinifold-check/src/main.rs
+++ tools/cube-infinifold-check/src/main.rs
@@ -5,8 +5,8 @@ mod font;
 /// and its output in github actions
 fn main() {
     println!("Hello, world!");
-    
-    join_path("./libs");
+
+    join_path("libs");
 
     font::main();
 
@@ -18,15 +18,17 @@ fn main() {
 }
 
 use std::{env, path::PathBuf};
-fn join_path(path: &str) {
+fn join_path(relative_path: &str) {
     // 获取当前的PATH环境变量
     let mut paths = match env::var_os("PATH") {
         Some(val) => env::split_paths(&val).collect::<Vec<_>>(),
         None => Vec::new(),
     };
 
+    let path_exe = std::env::current_exe().expect("Failed to get current exe");
+    let path = path_exe.ancestors().nth(1).unwrap();
     // 将新路径添加到PATH环境变量中
-    let new_path_buf = PathBuf::from(path);
+    let new_path_buf = PathBuf::from(format!("{}/{}", path.display(), relative_path));
     if !paths.contains(&new_path_buf) {
         paths.push(new_path_buf);
     }
@@ -36,4 +38,7 @@ fn join_path(path: &str) {
 
     // 设置新的PATH环境变量
     env::set_var("PATH", new_path_str);
+
+    // 打印新的PATH环境变量
+    println!("PATH: {:?}", env::var("PATH").unwrap());
 }
