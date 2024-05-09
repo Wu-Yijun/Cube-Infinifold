73074d7930582f9e2244356077f3fa63fef09df4
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-05-08 23:20:19 -0700

    test

--- examples/test_set_env/src/main.rs
+++ examples/test_set_env/src/main.rs
@@ -2,9 +2,9 @@ use std::env;
 
 use libloading::{library_filename, Library};
 
-fn set_path(){
+fn set_path(always_path: bool) {
     #[cfg(target_os = "windows")]
-    const PATH: &'static str = "PATH";
+    const PATH: &str = "PATH";
     #[cfg(target_os = "linux")]
     const PATH: &str = "LD_LIBRARY_PATH";
     #[cfg(target_os = "macos")]
@@ -12,28 +12,30 @@ fn set_path(){
     #[cfg(not(any(target_os = "windows", target_os = "linux", target_os = "macos")))]
     const PATH: &str = "?";
 
+    let PP: &str = if always_path { "PATH" } else { PATH };
+
     println!("Hello, world!");
-    let path = env::var_os(PATH).unwrap_or_default();
-    println!("{PATH}: {:?}", path);
+    let path = env::var_os(if always_path { "PATH" } else { PP }).unwrap_or_default();
+    println!("{PP}: {:?}", path);
 
     // add path environment variable
     let new_path = "libs";
     let new_path = format!("{}/{}", env::current_dir().unwrap().display(), new_path);
-    println!("Adding path to {PATH}: {new_path}");
+    println!("Adding path to {PP}: {new_path}");
     let new_path = std::path::PathBuf::from(new_path);
     let mut paths = env::split_paths(&path).collect::<Vec<_>>();
     if !paths.contains(&new_path) {
         paths.push(new_path);
     }
     let new_path = env::join_paths(paths).expect("Failed to join paths");
-    env::set_var(PATH, new_path);
-    println!("{PATH}: {:?}", env::var(PATH).unwrap());
+    env::set_var(PP, new_path);
+    println!("{PP}: {:?}", env::var(PP).unwrap());
 }
 
-unsafe fn load_lib(){
+unsafe fn load_lib() {
     let lib = Library::new(library_filename("add")).unwrap();
     let add: libloading::Symbol<fn(i32, i32) -> i32> = lib.get(b"add").unwrap();
-    
+
     println!("Calling add(1, 2)");
     let result = add(1, 2);
     println!("Result: {}", result);
@@ -42,6 +44,7 @@ unsafe fn load_lib(){
 }
 
 fn main() {
-    set_path();
+    set_path(false);
+    set_path(true);
     unsafe { load_lib() };
 }
