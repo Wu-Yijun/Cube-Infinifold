4542e209d5d83717ea12a17ac4f044705cfd7af0
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-05-09 00:37:11 -0700

    test

--- examples/test_set_env/src/lib.rs
+++ examples/test_set_env/src/lib.rs
@@ -1,5 +1,36 @@
+use std::env;
+
+use libloading::{library_filename, Library};
+
 #[no_mangle]
-pub fn add(a: i32, b: i32) -> i32 {
-    println!("Adding {} and {}", a, b);
-    a + b
+fn main() {
+    println!("\nHello, main fun!");
+    print_path();
+
+    load_add();
+}
+
+fn print_path() {
+    #[cfg(target_os = "windows")]
+    const PATH: &str = "PATH";
+    #[cfg(target_os = "linux")]
+    const PATH: &str = "LD_LIBRARY_PATH";
+    #[cfg(target_os = "macos")]
+    const PATH: &str = "DYLD_FALLBACK_LIBRARY_PATH";
+    #[cfg(not(any(target_os = "windows", target_os = "linux", target_os = "macos")))]
+    const PATH: &str = "?";
+
+    let path = env::var_os(PATH).unwrap_or_default();
+    println!("{PATH}: {:?}", path);
+}
+
+fn load_add() {
+    let lib = unsafe { Library::new(library_filename("add")).unwrap() };
+    let add: libloading::Symbol<fn(i32, i32) -> i32> = unsafe { lib.get(b"add").unwrap() };
+
+    println!("Calling add(1, 2)");
+    let result = add(1, 2);
+    println!("Result: {}", result);
+
+    lib.close().unwrap();
 }
--- examples/test_set_env/src/main.rs
+++ examples/test_set_env/src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 
 use libloading::{library_filename, Library};
 
-fn set_path(always_path: bool) {
+fn set_path() {
     #[cfg(target_os = "windows")]
     const PATH: &str = "PATH";
     #[cfg(target_os = "linux")]
@@ -12,44 +12,30 @@ fn set_path(always_path: bool) {
     #[cfg(not(any(target_os = "windows", target_os = "linux", target_os = "macos")))]
     const PATH: &str = "?";
 
-    let PP: &str = if always_path { "PATH" } else { PATH };
-
-    println!("Hello, world!");
-    let path = env::var_os(if always_path { "PATH" } else { PP }).unwrap_or_default();
-    println!("{PP}: {:?}", path);
+    let path = env::var_os(PATH).unwrap_or_default();
+    println!("{PATH}: {:?}", path);
 
     // add path environment variable
     let new_path = "libs/";
     let new_path = format!("{}/{}", env::current_dir().unwrap().display(), new_path);
-    println!("Adding path to {PP}: {new_path}");
+    println!("Adding path to {PATH}: {new_path}");
     let new_path = std::path::PathBuf::from(new_path);
     let mut paths = env::split_paths(&path).collect::<Vec<_>>();
     if !paths.contains(&new_path) {
         paths.push(new_path);
     }
     let new_path = env::join_paths(paths).expect("Failed to join paths");
-    env::set_var(PP, new_path);
-    println!("{PP}: {:?}", env::var(PP).unwrap());
-    
-}
-
-unsafe fn load_lib() {
-    let lib = Library::new(library_filename("add")).unwrap();
-    let add: libloading::Symbol<fn(i32, i32) -> i32> = lib.get(b"add").unwrap();
-
-    println!("Calling add(1, 2)");
-    let result = add(1, 2);
-    println!("Result: {}", result);
-
-    lib.close().unwrap();
+    env::set_var(PATH, new_path);
+    println!("{PATH}: {:?}", env::var(PATH).unwrap());
 }
 
 fn main() {
-    let output = std::process::Command::new("export LD_LIBRARY_PATH=:/home/runner/work/Cube-Infinifold/Cube-Infinifold/target/debug/libs")
-        .output()
-        .expect("Failed to execute command");
-    println!("Output: {}", String::from_utf8_lossy(&output.stdout));
-    // set_path(false);
-    // set_path(true);
-    unsafe { load_lib() };
+    set_path();
+    let path = library_filename("libs/main");
+    // let path = format!("libs/{:?}", library_filename("main"));
+    println!("Loading library: {:?}", path);
+    let lib = unsafe { Library::new(path).unwrap() };
+    let main = unsafe { lib.get::<fn()>(b"main").unwrap() };
+    main();
+    lib.close().unwrap();
 }
