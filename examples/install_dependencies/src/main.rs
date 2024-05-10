02dfdaa944f1fe9d9ed4b9509c21b0039f34f73b
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-05-09 22:19:18 -0700

    test

--- examples/install_dependencies/src/main.rs
+++ examples/install_dependencies/src/main.rs
@@ -1,35 +1,30 @@
 use std::process::Command;
 
-
-
 fn main() {
     // 检查系统中是否已经安装了 ffmpeg
     match Command::new("ffmpeg").arg("-version").output() {
-        Ok(output) => {
-            if output.status.success() {
-                // 如果已经安装，则打印 "already installed"
-                println!("ffmpeg is already installed.");
-            } else {
-                // 如果没有安装，则安装 ffmpeg
-                println!("Installing ffmpeg...");
-                // 根据你的系统和包管理器，使用适当的安装命令
-                // 这里是以 Debian/Ubuntu 系统为例
-                let install_output = Command::new("sudo")
-                    .arg("apt")
-                    .arg("install")
-                    .arg("ffmpeg")
-                    .output()
-                    .expect("Failed to execute command");
-
-                if install_output.status.success() {
-                    println!("ffmpeg installed successfully.");
-                } else {
-                    println!("Failed to install ffmpeg.");
-                }
-            }
+        Ok(output) if output.status.success() => {
+            // 如果已经安装，则打印 "already installed"
+            println!("ffmpeg is already installed.");
         }
-        Err(_) => {
+        _ => {
             println!("Failed to execute ffmpeg command. Make sure ffmpeg is installed.");
+            // 如果没有安装，则安装 ffmpeg
+            println!("Installing ffmpeg...");
+            // 根据你的系统和包管理器，使用适当的安装命令
+            // 这里是以 Debian/Ubuntu 系统为例
+            let install_output = Command::new("sudo")
+                .arg("apt")
+                .arg("install")
+                .arg("ffmpeg")
+                .output()
+                .expect("Failed to execute command");
+
+            if install_output.status.success() {
+                println!("ffmpeg installed successfully.");
+            } else {
+                println!("Failed to install ffmpeg.");
+            }
         }
     }
 }
